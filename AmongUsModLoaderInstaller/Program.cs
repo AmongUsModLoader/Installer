b297579859b1806910ec77fbd2e80f80f42acc6d
Ashley Wright <ahmedtrg981106@gmail.com>
2020-11-01 03:17:53 +0300

    Include correct projects

--- AmongUsModLoaderInstaller/Program.cs
+++ AmongUsModLoaderInstaller/Program.cs
@@ -6,153 +6,159 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Runtime.InteropServices;
-using System.Text.Json;
-using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using Gtk;
+using Newtonsoft.Json;
 
 namespace AmongUsModLoaderInstaller
 {
     internal static class Program
     {
         private static readonly bool IsLinux = RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
-        
+
         private static void Main()
         {
             var stream = typeof(Program).Assembly.GetManifestResourceStream("AmongUsModLoaderInstaller.Window.glade");
             if (stream == null) return;
-            using var reader = new StreamReader(stream);
-            Application.Init();
-            var builder = new Builder();
-            builder.AddFromString(reader.ReadToEnd());
-            var window = Get<ApplicationWindow>("main_container");
-            var typeSelector = Get<ComboBox>("type_selector");
-            var path = Get<FileChooserButton>("path");
-            var pathText = Get<Label>("path_label");
-            var prefixPath = Get<FileChooserButton>("wine_prefix");
-            var prefixText1 = Get<Label>("wine_prefix_label1");
-            var prefixText2 = Get<Label>("wine_prefix_label2");
-            var installButton = Get<Button>("install_button");
-            var clientPathText = pathText.Text;
-            var winePrefix1 = prefixText1.Text;
-            var winePrefix2 = prefixText2.Text;
-            var server = false;
-
-            T Get<T>(string name) where T : Widget => (T) builder.GetObject(name);
-
-            void SetText(bool steam)
+            using (var reader = new StreamReader(stream))
             {
-                if (steam)
-                {
-                    prefixText1.Text = "";
-                    prefixText2.Text = "Steam Directory";
-                }
-                else
-                {
-                    prefixText1.Text = winePrefix1;
-                    prefixText2.Text = winePrefix2;
-                }
-            }
+                Application.Init();
+                var builder = new Builder();
+                builder.AddFromString(reader.ReadToEnd());
+                var window = Get<ApplicationWindow>("main_container");
+                var typeSelector = Get<ComboBox>("type_selector");
+                var path = Get<FileChooserButton>("path");
+                var pathText = Get<Label>("path_label");
+                var prefixPath = Get<FileChooserButton>("wine_prefix");
+                var prefixText1 = Get<Label>("wine_prefix_label1");
+                var prefixText2 = Get<Label>("wine_prefix_label2");
+                var installButton = Get<Button>("install_button");
+                var clientPathText = pathText.Text;
+                var winePrefix1 = prefixText1.Text;
+                var winePrefix2 = prefixText2.Text;
+                var server = false;
 
-            const string relativeGameSteamLocation = "steamapps/common/Among Us/";
-            var steamCheck = Get<CheckButton>("steam_check");
-            steamCheck.Active = true;
-            
-            ToggleHandler(window, EventArgs.Empty);
-            steamCheck.Toggled += ToggleHandler;
-            prefixPath.CurrentFolderChanged += (sender, args) =>
-            {
-                if (steamCheck.Active)
-                {
-                    path.SetCurrentFolder(prefixPath.CurrentFolder + "/" + relativeGameSteamLocation);
-                }
-            };
+                T Get<T>(string name) where T : Widget => (T) builder.GetObject(name);
 
-            typeSelector.Changed += (sender, args) =>
-            {
-                if (!typeSelector.GetActiveIter(out var iter)) return;
-                var value = (string) typeSelector.Model.GetValue(iter, 0);
-                if (value == "Server")
+                void SetText(bool steam)
                 {
-                    server = true;
-                    pathText.Text = "Installation Directory";
-                    prefixPath.Hide();
-                    prefixText1.Hide();
-                    prefixText2.Hide();
-                    steamCheck.Hide();
-                }
-                else
-                {
-                    server = false;
-                    pathText.Text = clientPathText;
-                    steamCheck.Show();
-                    if (!steamCheck.Active && !IsLinux) return;
-                    prefixPath.Show();
-                    prefixText1.Show();
-                    prefixText2.Show();
-                }
-            };
-            
-            void ToggleHandler(object? sender, EventArgs args)
-            {
-                if (steamCheck.Active)
-                {
-                    var steam = IsLinux
-                        ? Environment.GetEnvironmentVariable("HOME") + "/.local/share/Steam/"
-                        : Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86) + "/Steam/";
-                    path.SetCurrentFolder(steam + relativeGameSteamLocation);
-
-                    if (IsLinux)
+                    if (steam)
                     {
-                        prefixText1.Show();
-                        prefixText2.Show();
-                        SetText(true);
+                        prefixText1.Text = "";
+                        prefixText2.Text = "Steam Directory";
                     }
                     else
                     {
+                        prefixText1.Text = winePrefix1;
+                        prefixText2.Text = winePrefix2;
+                    }
+                }
+
+                const string relativeGameSteamLocation = "steamapps/common/Among Us/";
+                var steamCheck = Get<CheckButton>("steam_check");
+                steamCheck.Active = true;
+
+                ToggleHandler(window, EventArgs.Empty);
+                steamCheck.Toggled += ToggleHandler;
+                prefixPath.CurrentFolderChanged += (sender, args) =>
+                {
+                    if (steamCheck.Active)
+                    {
+                        path.SetCurrentFolder(prefixPath.CurrentFolder + "/" + relativeGameSteamLocation);
+                    }
+                };
+
+                typeSelector.Changed += (sender, args) =>
+                {
+                    if (!typeSelector.GetActiveIter(out var iter)) return;
+                    var value = (string) typeSelector.Model.GetValue(iter, 0);
+                    if (value == "Server")
+                    {
+                        server = true;
+                        pathText.Text = "Installation Directory";
+                        prefixPath.Hide();
                         prefixText1.Hide();
                         prefixText2.Hide();
-                        prefixPath.Hide();
+                        steamCheck.Hide();
                     }
-                    prefixPath.SetCurrentFolder(steam);
-                }
-                else
-                {
-                    path.UnselectAll();
-                    path.Show();
-                    pathText.Show();
-
-                    if (IsLinux)
+                    else
                     {
-                        SetText(false);
-                        prefixText1.Show();
-                        prefixText2.Show();
+                        server = false;
+                        pathText.Text = clientPathText;
+                        steamCheck.Show();
+                        if (!steamCheck.Active && !IsLinux) return;
                         prefixPath.Show();
-                        prefixPath.SetCurrentFolder(Environment.GetEnvironmentVariable("HOME") + "/.wine/");
+                        prefixText1.Show();
+                        prefixText2.Show();
+                    }
+                };
+
+                void ToggleHandler(object sender, EventArgs args)
+                {
+                    if (steamCheck.Active)
+                    {
+                        var steam = IsLinux
+                            ? Environment.GetEnvironmentVariable("HOME") + "/.local/share/Steam/"
+                            : Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86) + "/Steam/";
+                        path.SetCurrentFolder(steam + relativeGameSteamLocation);
+
+                        if (IsLinux)
+                        {
+                            prefixText1.Show();
+                            prefixText2.Show();
+                            SetText(true);
+                        }
+                        else
+                        {
+                            prefixText1.Hide();
+                            prefixText2.Hide();
+                            prefixPath.Hide();
+                        }
+
+                        prefixPath.SetCurrentFolder(steam);
                     }
                     else
                     {
-                        prefixText1.Hide();
-                        prefixText2.Hide();
-                        prefixPath.Hide();
+                        path.UnselectAll();
+                        path.Show();
+                        pathText.Show();
+
+                        if (IsLinux)
+                        {
+                            SetText(false);
+                            prefixText1.Show();
+                            prefixText2.Show();
+                            prefixPath.Show();
+                            prefixPath.SetCurrentFolder(Environment.GetEnvironmentVariable("HOME") + "/.wine/");
+                        }
+                        else
+                        {
+                            prefixText1.Hide();
+                            prefixText2.Hide();
+                            prefixPath.Hide();
+                        }
                     }
                 }
+
+                installButton.Clicked += async (sender, args) =>
+                {
+                    try
+                    {
+                        await Install(server, steamCheck.Active, path.CurrentFolder, prefixPath.CurrentFolder);
+                    }
+                    catch (Exception e)
+                    {
+                        using (var dialog = new MessageDialog(window, DialogFlags.Modal, MessageType.Error,
+                            ButtonsType.Close, "{0}", e.Message))
+                        {
+                            dialog.Run();
+                        }
+                    }
+                };
+                window.DeleteEvent += (sender, args) => Application.Quit();
+                window.ShowAll();
             }
 
-            installButton.Clicked += async (sender, args) =>
-            {
-                try
-                {
-                    await Install(server, steamCheck.Active, path.CurrentFolder, prefixPath.CurrentFolder);
-                }
-                catch (Exception e)
-                {
-                    using var dialog = new MessageDialog(window, DialogFlags.Modal, MessageType.Error, ButtonsType.Close, "{0}", e.Message);
-                    dialog.Run();
-                }
-            };
-            window.DeleteEvent += (sender, args) => Application.Quit();
-            window.ShowAll();
             Application.Run();
         }
 
@@ -168,7 +174,7 @@ namespace AmongUsModLoaderInstaller
                 if (IsLinux)
                 {
                     if (steam) runDir += "/steamapps/compatdata/945360/pfx/";
-                    
+
                     //TODO this doesn't check if they key already exists
                     Process.Start(new ProcessStartInfo("/usr/bin/wine",
                         "REG ADD HKEY_CURRENT_USER\\Software\\Wine\\DllOverrides /v winhttp /t REG_SZ /d native,builtin")
@@ -178,59 +184,60 @@ namespace AmongUsModLoaderInstaller
                     });
                 }
 
-                using var client = new HttpClient();
-                client.DefaultRequestHeaders.UserAgent.TryParseAdd("request");
-
-                var url = JsonSerializer
-                    .Deserialize<GithubRelease>(
-                        await client.GetStringAsync("https://api.github.com/repos/BepInEx/BepInEx/releases/latest"))
-                    ?.Url;
-
-                var prop = JsonSerializer.Deserialize<AssetsProp>(await client.GetStringAsync(url));
-
-                var x86Url = (from downloadableProp in prop.Assets where downloadableProp.Name.Contains("x86") select downloadableProp.Url).FirstOrDefault();
-
-                if (!gameDir.EndsWith("/")) gameDir += "/";
-
-                string zip = gameDir + "BepInEx.zip";
-
-                if (x86Url == null)
+                using (var client = new HttpClient())
                 {
-                    //TODO tell the user that the x86 file for bepinex wasnt found and make them select it from the prop.Assets
-                }
-                else
-                {
-                    var response = await client.GetAsync(x86Url);
-                    await using (var fs = File.OpenWrite(zip))
+                    client.DefaultRequestHeaders.UserAgent.TryParseAdd("request");
+
+                    var url = JsonConvert.DeserializeObject<GithubRelease>(
+                            await client.GetStringAsync("https://api.github.com/repos/BepInEx/BepInEx/releases/latest"))
+                        .Url;
+
+                    var prop = JsonConvert.DeserializeObject<AssetsProp>(await client.GetStringAsync(url));
+
+                    var x86Url =
+                        (from downloadableProp in prop.Assets
+                            where downloadableProp.Name.Contains("x86")
+                            select downloadableProp.Url).FirstOrDefault();
+
+                    if (!gameDir.EndsWith("/")) gameDir += "/";
+
+                    var zip = gameDir + "BepInEx.zip";
+
+                    if (x86Url == null)
                     {
-                        await response.Content.CopyToAsync(fs);
+                        //TODO tell the user that the x86 file for bepinex wasnt found and make them select it from the prop.Assets
                     }
-                    
-                    ZipFile.ExtractToDirectory(zip, gameDir + "/");
-                    File.Delete(zip);
+                    else
+                    {
+                        var response = await client.GetAsync(x86Url);
+                        using (var fs = File.OpenWrite(zip))
+                        {
+                            await response.Content.CopyToAsync(fs);
+                        }
+
+                        ZipFile.ExtractToDirectory(zip, gameDir + "/");
+                        File.Delete(zip);
+                    }
                 }
             }
         }
 
         private class GithubRelease
         {
-            [JsonPropertyName("url")]
-            public string Url { get; set; }
+            [JsonProperty("url")] public string Url { get; set; }
         }
-        
+
         private class AssetsProp
         {
-            [JsonPropertyName("assets")]
-            public DownloadableProp[] Assets { get; set; }
+            [JsonProperty("assets")] public DownloadableProp[] Assets { get; set; }
         }
-        
+
         private class DownloadableProp
         {
-            [JsonPropertyName("browser_download_url")]
+            [JsonProperty("browser_download_url")]
             public string Url { get; set; }
-            
-            [JsonPropertyName("name")]
-            public string Name { get; set; }
+
+            [JsonProperty("name")] public string Name { get; set; }
         }
     }
 }
